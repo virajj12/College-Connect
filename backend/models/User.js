a31ab7feb9b735f90597960c8686dbd06d9187df
virajj12 <vjvirajjain122005@gmail.com>
2025-10-23 22:57:42 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,7 +1,7 @@
-// models/User.js
 const mongoose = require('mongoose');
+const crypto = require('crypto'); // Core Node.js module
 
-const UserSchema = new mongoose.Schema({
+const UserSchema = new new mongoose.Schema({
     email: {
         type: String,
         required: true,
@@ -23,7 +23,27 @@ const UserSchema = new mongoose.Schema({
     },
     year: {
         type: String
-    }
+    },
+    // --- ADDED FOR PASSWORD RESET ---
+    resetPasswordToken: String,
+    resetPasswordExpire: Date
 });
+
+// Method to generate and hash the password token (used by /forgot-password)
+UserSchema.methods.getResetPasswordToken = function() {
+    const resetToken = crypto.randomBytes(20).toString('hex');
+    
+    // Hash the token and set to resetPasswordToken field (for storage in DB)
+    this.resetPasswordToken = crypto
+        .createHash('sha256')
+        .update(resetToken)
+        .digest('hex');
+
+    // Set token expiration to 1 hour
+    this.resetPasswordExpire = Date.now() + 60 * 60 * 1000;
+
+    // Return the plain-text token (for the email link)
+    return resetToken;
+};
 
 module.exports = mongoose.model('User', UserSchema);
